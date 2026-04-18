c967d67cf3c531c21a4f611f5becf757a026d7b6
sanumajuder123-source <sanumajuder123@gmail.com>
2026-04-18 15:29:01 +0530

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,20 @@
+export default {
+  expo: {
+    name: "Bhakti Japa App",
+    slug: "bhakti-japa-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    android: {
+      package: "com.sanu.bhaktijapaapp",
+      adaptiveIcon: {
+        backgroundColor: "#ffffff"
+      }
+    },
+    extra: {
+      eas: {
+        projectId: "28bda98c-21ce-4233-8a26-133a6788b78a"
+      }
+    }
+  }
+};
